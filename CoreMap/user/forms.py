132092d4a0ef681675ef89996a50a707244c2472
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2023-12-15 21:30:35 +0530

    added-formatter

--- CoreMap/user/forms.py
+++ CoreMap/user/forms.py
@@ -53,6 +53,5 @@ class ProfileForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ProfileForm, self).__init__(*args, **kwargs)
-
         for name, field in self.fields.items():
             field.widget.attrs.update({"class": "form-control item"})
